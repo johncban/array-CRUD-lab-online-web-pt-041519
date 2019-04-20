920ea64ff49c113ee5fcd63624fbb59d14271e31
johncban <johncban@gmail.com>
2019-04-20 00:06:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,38 @@
 def create_an_empty_array
-  
+  ar = []
 end
 
 def create_an_array
-  
+  ar = [1..10, "missile prep", "missile target acquisition", "begin countdown"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  element = "arrays!"
+  array = ["wow", "I", "am", "really", "learning"]
+  array << element
+  puts array.last
 end
 
 def add_element_to_start_of_array(array, element)
-  
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
